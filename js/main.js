fefeb4d286f6ee9d63fdf0c20d31b65f5888e58f
tishrich <richards.tishana@gmail.com>
2014-11-14 18:28:31 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$('.popInfo').on('click', function(){
+		$(this).parents('.inner').find('.hiddenInfo').fadeToggle(500);
+	});
+
+//    $("#qsearchmenu").click(function(){
+//    		$('#popout').show();
+
+// })
+
+
+});
